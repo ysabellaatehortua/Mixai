fbb7b81dd311e5d497257145cab718fd3fe0e55d
ysabellaatehortua <yatehort@oberlin.edu>
2021-12-07 16:08:15 -0500

    Create chromosome.py

---
+++ chromosome.py
@@ -0,0 +1,9 @@
+class Chromosome():
+
+    def __init__(self, liquor_types, liquor_amts, modifier_types, modifier_amts, mixer_types, mixer_amts):
+        self.liquor_types = liquor_types
+        self.liquor_amts = liquor_amts
+        self.modifier_types = modifier_types
+        self.modifier_amts = modifier_amts
+        self.mixer_types = mixer_types
+        self.mixer_amts = mixer_amts
