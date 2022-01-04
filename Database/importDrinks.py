52c88dce733523db79ba1369496b3b3f508fdd38
Henry Sieber <hsieber@oberlin.edu>
2022-01-03 21:50:26 -0500

    more work on importing and cleaning data.

--- Database/importDrinks.py
+++ Database/importDrinks.py
@@ -15,60 +15,63 @@ def main():
             # print()
             cocktails, ingredients = extractData(drink, cocktails, ingredients)
 
-def loadData():
+def loadData(): #get json data from website
     allDrinks = []
     for char in "1234567890abcdefghijklmnopqrstuvwxyz":
         section = []
         response = requests.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?f="+char)
         drinks = json.loads(response.text)
-        if drinks["drinks"]:
+        if drinks["drinks"]: 
             for i in range(len(drinks["drinks"])):
-                newDrinks = json.dumps(drinks["drinks"][i])
+                newDrinks = json.dumps(drinks["drinks"][i]) #store in sections for each letter
                 section += [newDrinks]
         allDrinks += [section]
     return allDrinks
 
 
-def extractData(drink, cocktails, ingredients):
+def extractData(drink, cocktails, ingredients): #get the data from each drink in more usable form
     newCocktails = [] 
     newIngredients = []
-    drink = json.load(drink)
-    drinkType = drink["strCategory"]
-    if (drinkType == "\"Cocktail\"" or drinkType == "\"Ordinary Drink\"" or drinkType == "\"Punch / Party Drink\""):
+    drink = json.loads(drink)
+    drinkType = drink["strCategory"] #make sure the type is not a shot or coffee or other weird drink for now
+    if (drinkType == "Cocktail" or drinkType == "Ordinary Drink" or drinkType == "Punch / Party Drink"):
         cleanedDrink = cleanDrink(drink, ingredients)
     cocktails += newCocktails
     ingredients += newIngredients
     return cocktails, ingredients
-
-def cleanDrink(drink, ingredients):
+ 
+def cleanDrink(drink, ingredients): #take each drink and extract the necessary info we need to feed it into the database
     drinkName = drink["strDrink"].strip("\'\"").lower
     alcoholic = (drink["strAlcoholic"] == "\"Alcoholic\"")
     glass = drink["strGlass"]
     measurements = []
     done = False
     j = 1
-    while not done and j < 16:
+    while not done and j < 16: #loop through all possible ingredients
         ingredientString = "strIngredient"+str(j)
         measurementString = "strMeasure"+str(j)
         ingredient = drink[ingredientString]
-        measurement = drink[measurementString]
+        measurement = drink[measurementString] #seperate ingredient and measurement and clean them both 
         if ingredient == "null" or ingredient == "":
             done = True
             continue
-        measurements += parseMeasurement(measurement)
-        ingredients += (ingredient)
+        if measurement and ingredient:
+            # measurements += parseMeasurement(measurement)
+            parseMeasurement(measurement)
+            ingredients += (ingredient)
         j+=1
     newDrink = {drinkName : [alcoholic, glass, [ingredients], [measurements]]}
     return newDrink
     
 
-def parseMeasurement(measure):
-    quantity = 0
-    unit = ""
-    cleanedMeasure = (quantity, unit) 
-    numberstr = re.search("^[0-9].*[0-9]$", measure)
-    print(numberstr)
-    return cleanedMeasure
+def parseMeasurement(measure): #this will be more or less what is done in testMeasurements
+    print(measure)
+    # quantity = 0
+    # unit = ""
+    # cleanedMeasure = (quantity, unit) 
+    # numberstr = re.search("^[0-9].*[0-9]$", measure)
+    # print(numberstr)
+    # return cleanedMeasure
     
            
 #print("there are", numDrinks, "cocktails in the database")
---
+++ Database/testMeasurements.py
@@ -0,0 +1,96 @@
+import re
+from fractions import Fraction
+
+
+
+
+def main():
+    with open('measurements.txt') as f: #import most of measurement data stored in text file for convenience
+        measurements = f.readlines()
+
+    newMeasurements = []
+    for measurment in measurements: 
+        numberstr = ""
+        done = True in [char.isdigit() for char in measurment] #split the number of the measurement from the unit of the measurement
+        seenNum = False
+        while done:
+            if measurment[0].isdigit():
+                seenNum = True
+            if seenNum:
+                numberstr += measurment[0]
+                measurment = measurment[1:]
+            else:
+                measurment = measurment[1:]
+            done = True in [char.isdigit() for char in measurment]
+        measurementstr = measurment.strip().lower()
+        measurementNum = numify(numberstr)
+        print(measurementNum)
+        curMeasurement = (numberstr, measurementstr)
+        newMeasurements += curMeasurement
+
+    # print(newMeasurements)
+
+
+def numify(measurementnum): 
+    if measurementnum == "70ml/2": #annoying edge case
+        num = 2
+    elif '/' in measurementnum:
+        num = float(sum(Fraction(s) for s in measurementnum.split())) #deal with different ways strings represent numbers 
+    elif '.' in measurementnum:
+        num = float(measurementnum)
+    elif '-' in measurementnum:
+        a,b = measurementnum.split("-")
+        num = float((int(a)+int(b))/2)
+    elif 'or' in measurementnum:
+        a,b = measurementnum.split("or")
+        num = float((int(a)+int(b))/2)
+    elif measurementnum == '':
+        num = ''
+    else:
+        num = float(measurementnum)
+    return num
+
+def convertToOz(measurementstr, num):
+    if measurementstr == 'oz' or measurementstr =='fl oz': #convert standard other measurements to fluid ounces
+        convertedNum = num
+    elif measurementstr =='tsp':
+        convertedNum = num/6
+    elif measurementstr =='tblsp':
+        convertedNum = num/2
+    elif measurementstr =='cup' or measurementstr =='cups':
+        convertedNum = num*8
+    elif measurementstr =='shots' or measurementstr =='shot' or measurementstr =='jigger' or measurementstr =='jiggers':
+        convertedNum = num*1.5
+    elif measurementstr =='pint' or measurementstr =='pints':
+        convertedNum = num*16
+    elif measurementstr =='qt':
+        convertedNum = num*32
+    elif measurementstr =='cl':
+        convertedNum = num/2.957
+    elif measurementstr =='ml':
+        convertedNum = num/29.574
+    elif measurementstr =='dl':
+        convertedNum = num*3.381
+    elif measurementstr =='gr':
+        convertedNum = num/29.574
+    elif measurementstr =='L':
+        convertedNum = num*33.814
+    elif measurementstr =='gal':
+        convertedNum = num*128
+    elif measurementstr =='lb':
+        convertedNum = num*15.34
+    elif measurementstr =='can':
+        convertedNum = num*12
+    return convertedNum
+
+
+    
+    #types:
+    # 
+    # proportions, dash, wedge, fill with, part, splash, fresh, parts, twist of, dashes, top up, drops, cubes, " ", fifth, small bottle, Juice of, slice, Squeeze, crushed, bottle, Float, Pinch, sprigs, stick, piece, Top, dashes, glass, chunks, cube, Top up with, Garnish, long strip, orange, mini, to taste, fill with, around rim aboout 1 pinch, pinches, (if needed), Turkish apple, Garnish with, inch, chunk, whole, handful, (Claret), Over, scoops, inch strips, Rimmed, Full Glass, Mikey bottle, large bottle, crushed, splashes, cans, Large Sprig, Measures, pods, About 8 Drops, full glass
+
+    
+
+
+
+main()
