7782a1aa6c52f62108dedf1da20b1debb86ace25
JHSieber <hsieber@oberlin.edu>
2022-01-21 18:25:29 -0500

    Update ALLOWED_HOSTS with site and development server URL

--- Mixai/settings.py
+++ Mixai/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['<murmuring-peak-97157.herokuapp.com','127.0.0.1']
 
 
 # Application definition
