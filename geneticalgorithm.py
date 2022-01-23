f2d745a6d1568b701d7e44e2bbe7619bce83a3d8
nataliekorzh <nkorzh@oberlin.edu>
2022-01-23 14:44:29 -0500

    restructuring

--- geneticalgorithm.py
+++ geneticalgorithm.py
@@ -79,37 +79,6 @@ class GeneticAlgorithm():
             line = f.readline().rstrip()
             #line = f.readline()
             #print(line)
-    """
-    def ingredient_deets(self):
-        #self.ingredient_details['alcohol'] = {'dark rum': 3, 'light rum': 3, 'vodka': 1, 'gin': 1, 'tequila': 1, 'peach vodka': 2, 'vanilla vodka': 2, 'absolut citron': 2, 'absolut vodka': 1, 'applejack': 4, 'vermouth': 0.5, 'scotch': 0.5, 'sweet vermouth': 0.5, 'dry vermouth': 0.5, 'blended whiskey': 0.5, 'bourbon': -0.5, 'blackberry brandy': 4, 'champagne': 0.1, 'rye whiskey': 0.5, 'rum': 3, '151 proof rum': 0.25, 'sloe gin': 1, 'cherry brandy': 4, 'spiced rum': 3, 'port': 4, 'brandy': 4, 'lillet blanc': 0.5, 'pisco': 0.5, 'dubonnet rouge': 0.5, 'absinthe': 0.25, 'apricot brandy': 4, 'cachaca': 3, 'mezcal': 1, 'cognac': 0.5, 'malibu rum': 2, 'white rum': 3, 'whiskey': 0.5, 'irish whiskey': 0.5, 'peach brandy': 4, 'jack daniels': 0.5, 'apple brandy': 4, 'cranberry vodka': 2, 'beer': .1, 'southern comfort': 2, 'blended scotch': 0.5, 'islay single malt scotch': 0.5, 'everclear': 0.25, 'prosecco': 1, 'coffee brandy': 4, 'lime vodka': 2, 'red wine': 0.1, 'ricard': 0.5, 'ruby port': 4, 'anis': 0.25, 'rosso vermouth': 0.5, 'gold rum': 3, 'pernod': 0.25, 'jägermeister': 3, 'zima': 0.1}
-        #self.ingredient_details['mixer'] = {'sweet': ['orange juice', 'coca-cola', 'cranberry juice',  'tonic water', 'grapefruit juice', 'peach nectar', 'pineapple juice', 'passion fruit juice', 'hot chocolate',  'ginger ale', 'ginger beer',  'pomegranate juice',  'lemonade', 'iced tea', 'schweppes russchian', 'apple juice',  'grape soda', 'coconut milk', '7-up', 'root beer', 'coca cola', 'cream soda'], 'nonsweet': ['milk', 'tomato juice', 'coffee'], 'neutral': ['carbonated water', 'water', 'soda water']}
-        #self.ingredient_details['modifier'] = {'sweet':['roses sweetened lime juice', 'sugar', 'sugar syrup', 'demerara sugar', 'pineapple syrup', 'vanilla syrup', 'rose', 'strawberries', 'honey', 'apricot nectar', 'powdered sugar',  'maraschino cherry', 'aperol', 'orgeat syrup',  'honey syrup',  'pineapple', 'passion fruit syrup',  'raspberry syrup', 'blood orange',  'maple syrup'],'neutral': ['heavy cream', 'light cream', 'espresso', 'egg', 'sweet and sour', 'ginger syrup', 'allspice', 'salt'],'sour': ['lemon juice', 'lime juice', 'sour mix', 'fresh lime juice', 'lime']}
-        self.ingredient_details['alcohol'] = {}
-        self.ingredient_details['alcohol']['dark'] = ['scotch', 'blended whiskey', 'bourbon', 'rye whiskey', 'blended scotch', 'islay single malt scotch', 'cognac', 'whiskey', 'irish whiskey', 'jack daniels']
-        self.ingredient_details['alcohol']['clear'] = ['vodka', 'tequila', 'absolut vodka',  'mezcal']
-        self.ingredient_details['alcohol']['sweet'] = ['gin', 'peach vodka', 'vanilla vodka', 'absolut citron', 'malibu rum', 'lime vodka', 'cranberry vodka', 'dark rum', 'light rum', 'white rum', 'rum', 'gold rum', 'spiced rum', 'cachaca', 'jägermeister']
-        self.ingredient_details['alcohol']['bitter'] = ['red wine', 'vermouth', 'white wine', 'beer', 'champagne', 'prosecco', 'rose']
-        
-        self.ingredient_details['mixer'] = {}
-        self.ingredient_details['mixer']['sweet'] = ['orange juice', 'coca-cola', 'tonic water', 'peach nectar', 'pineapple juice', 'ginger ale', 'ginger beer', 'apple juice',  'grape soda', '7-up', 'root beer', 'red gatorade', 'blue gatorade', 'yellow gatorade', 'orange gatorade', 'red bull', 'monster', 'rockstar', 'bang', 'cream soda']
-        self.ingredient_details['mixer']['sour'] = ['passionfruit juice', 'pomegranate juice', 'lemonade']
-        self.ingredient_details['mixer']['bitter'] = ['cranberry juice', 'grapefruit juice', 'iced tea', 'coffee']
-        self.ingredient_details['mixer']['weird'] = ['hot chocolate', 'coconut milk', 'coffee', 'tomato juice']
-        self.ingredient_details['mixer']['neutral'] = ['carbonated water', 'water', 'soda water']
-        
-        self.ingredient_details['modifier'] = {}
-        self.ingredient_details['modifier']['sweet'] = ['sugar', 'sugar syrup', 'pineapple syrup', 'strawberries', 'honey', 'apricot nectar', 'powdered sugar',  'maraschino cherry',  'honey syrup',  'pineapple', 'passion fruit syrup',  'raspberry syrup', 'maple syrup', 'grenadine']
-        self.ingredient_details['modifier']['sour'] = ['lemon juice', 'lime juice', 'sour mix', 'fresh lime juice', 'lime', 'blood orange']
-        self.ingredient_details['modifier']['bitter'] = ['espresso']
-        self.ingredient_details['modifier']['weird'] = ['heavy cream', 'light cream', 'allspice', 'rose', 'sweet and sour']
-        self.ingredient_details['modifier']['neutral'] = ['egg', 'ginger syrup']
-        self.ingredient_details['modifier']['salty']= ['salt']
-
-        self.ingredient_details['liqueur'] = {}
-        self.ingredient_details['liqueur']['sweet'] = ['peach schnapps', 'blue curacao', 'midori melon liqueur', 'melon liqueur', 'coconut liqueur',  'cherry liqueur', 'triple sec', 'elderflower cordial', 'cointreau', 'raspberry liqueur']
-        self.ingredient_details['liqueur']['bitter'] = ['coffee liqueur', 'campari', 'passoa']
-        self.ingredient_details['liqueur']['weird'] = ['kahlua', 'baileys irish cream', 'amaretto']
-    """
 
     def train_input_setup(self, population):
         drinks = []
@@ -265,7 +234,11 @@ class GeneticAlgorithm():
     
     def gen_alg(self):
         n = 0
-        while n < 5:
+        avg = []
+        avg.append(sum(self.train_output) / len(self.train_output))
+        distribution = []
+        distribution.append(self.train_output)
+        while n < 25:
             new_population = []
             pop_split = random.randint(0,len(self.population))
             #print(pop_split)
@@ -296,13 +269,12 @@ class GeneticAlgorithm():
             self.train_input = train_in
             self.train_output = train_out
             n += 1
+            avg.append(sum(self.train_output) / len(self.train_output))
+            distribution.append(self.train_output)
+        return avg, distribution
 
     def create_fitness_func(self):
         self.predictor.fit(X=self.train_input, y=self.train_output)
-        #test = [[0,2,0,0,0,0,10,0,0,0,0,0,0,0,0,0.5,0,0,0,0,1]]
-        #outcome = self.predictor.predict(X=test)
-        #coefficients = self.predictor.coef_
-        #print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
 
     def parse_ingredients(self, users_list):
         user_good = {}
@@ -329,19 +301,7 @@ def main():
     gen_alg.population_setup()
     gen_alg.make_initial_io()
     gen_alg.create_fitness_func()
-    gen_alg.gen_alg()
-    l = ['malibu rum', 'vodka', 'tonic water', 'grapefruit juice', 'apple juice', 'lime juice', 'sugar', '7-up', 'sprite']
-    ug = gen_alg.parse_ingredients(l)
-    ug = sorted(ug.items())
-    #print(ug)
-    for num in ug:
-        if num[0] < 2:
-            print(str(num[0]) + " ingredients needed:\n" + str(num[1]) + "\n")
-        
-    #for i in range(25):#range(len(gen_alg.population)):
-        #print(gen_alg.population[i].recipe())
-        #print(gen_alg.train_output[i])
-        #print('\n')
+    avg, distrib = gen_alg.gen_alg()
 
 if __name__ == "__main__":
     main()
